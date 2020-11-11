049a486a88781c2fd25dda285543b04978cdfb21
Ivgesha Lag <savchenko2002je@gmail.com>
2020-11-11 10:40:19 +0300

    Создание пустых классов

--- MenuDefs.cpp
+++ MenuDefs.cpp
@@ -38,11 +38,11 @@ void initLoginMenu()
 		password = ((MenuElementEditField *)(*it))->getInput();
 		// TODO: Здесь будут проверки данных
 		// TODO: Функция сама сообщит об успешности входа.
-		MainTypes::User::login_(login, password);
-		if (MainTypes::User::getActive())
+		User::login_(login, password);
+		if (User::getActive())
 		{
 			Menu::multiPopMenuStack(1);
-			if (MainTypes::User::getActive()->getIsAdmin())
+			if (User::getActive()->getIsAdmin())
 			{
 				adminMenu->addToStack();
 			}
@@ -83,7 +83,8 @@ void initRegisterMenu()
 		repeatPassword = ((MenuElementEditField *)(*it))->getInput();
 		// TODO: Здесь будут проверки данных
 		// TODO: Функция регистрации сама выводит сообщения пользователю.
-		MainTypes::User::register_(login, password, repeatPassword);
+		User::register_(login, password, repeatPassword);
+		registerMenu->reset();
 		Menu::multiPopMenuStack(1);
 	});
 	NME_FUNC_BUTTON("Назад", []() {
--- User.h
+++ User.h
@@ -1,43 +1,10 @@
 #pragma once
 #include <string>
 #include "Encrypt.h"
+#include "Person.h"
 using namespace std;
 
-namespace MainTypes
+class User :
+	public Person
 {
-	class User;
-
-	class User
-	{
-	private:
-		static User* active;
-		string login;
-		string encryptedPassword;
-		string firstName;
-		string lastName;
-		bool isAdmin;
-	public:
-		// Создание и разрушение
-		User(string login, string password, bool isAdmin, string firstName, string lastName) :
-			login(login), encryptedPassword(Utils::encrypt(password)), isAdmin(isAdmin), firstName(firstName), lastName(lastName) {};
-		~User();
-
-		// Интерфейс
-		static void setActive(User* user) { active = user; };
-		static User* getActive() { return active; };
-
-		void setIsAdmin(bool isAdmin) { this->isAdmin = isAdmin; };
-		void setFirstName(string firstName) { this->firstName = firstName; };
-		void setLastName(string firstName) { this->lastName = lastName; };
-		void setEncryptedPassword(string password) { encryptedPassword = Utils::encrypt(password); };
-
-		bool getIsAdmin() { return isAdmin; }
-		string getFirstName() { return firstName; };
-		string getLastName() { return lastName; };
-		string getEncryptedPassword(string password) { return encryptedPassword; };
-		string getLogin() { return login; };
-
-		static void login_(string login, string password);
-		static void register_(string login, string password, string repeatPassword);
-	};
-}
+};
