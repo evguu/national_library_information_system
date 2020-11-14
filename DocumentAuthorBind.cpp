26efe6c1b5df7307f0f0258ea34a5306fa7dcf9c
Ivgesha Lag <savchenko2002je@gmail.com>
2020-11-14 21:56:55 +0300

    Класс связи документов и авторов

--- DocumentAuthorBind.cpp
+++ DocumentAuthorBind.cpp
@@ -1,2 +1,30 @@
 #include "pch.h"
 #include "DocumentAuthorBind.h"
+
+DocumentAuthorBind * DocumentAuthorBind::loadRecord(ifstream & fin)
+{
+	string document;
+	string author;
+	getline(fin, document);
+	getline(fin, author);
+	if (fin.eof() || fin.fail()) return nullptr;
+	for (auto document_it : Document::getBinder().getRecords())
+	{
+		if (document_it->getId() == stoi(document))
+		{
+			for (auto author_it : Author::getBinder().getRecords())
+			{
+				if (author_it->getId() == stoi(author))
+				{
+					return new DocumentAuthorBind(document_it, author_it);
+				}
+			}
+		}
+	}
+	return (DocumentAuthorBind*)Utils::FBV_CORRUPTED_DATA;
+}
+
+void DocumentAuthorBind::saveRecord(ofstream & fout)
+{
+	fout << document->getId() << endl << author->getId() << endl;
+}
--- DocumentAuthorBind.h
+++ DocumentAuthorBind.h
@@ -1,15 +1,24 @@
 #pragma once
 #include "Document.h"
 #include "Author.h"
+#include "FileBindedVector.h"
+#include "CharacterSets.h"
+using namespace std;
+
 class DocumentAuthorBind
 {
 private:
 	Document* document;
 	Author* author;
+	static Utils::FileBindedVector<DocumentAuthorBind> binder;
 public:
 	DocumentAuthorBind(Document* document, Author* author) : document(document), author(author) {};
 	~DocumentAuthorBind() {};
 	auto& getDocument() { return document; };
 	auto& getAuthor() { return author; };
+
+	static auto& getBinder() { return binder; };
+	static DocumentAuthorBind* loadRecord(ifstream& fin);
+	void saveRecord(ofstream& fout);
 };
 
